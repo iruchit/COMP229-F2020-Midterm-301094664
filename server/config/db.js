1b76f7db772d230d4fd0ad028d97b85af74d2d5c
iruchit <rhpatel1605@gmail.comibb>
2020-10-28 17:31:43 +0530

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/book_store"
+  "URI": "mongodb+srv://Ruchit:yeA7WBaxyuwc2KW6@mongodbserver.cgh6f.mongodb.net/books_store01?retryWrites=true&w=majority"
+};
